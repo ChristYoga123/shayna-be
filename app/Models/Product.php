57e69d8ede98d5e174b041b9ad381711003ad713
ChristYoga123 <christianuswibisono@gmail.com>
2022-12-21 00:20:34 +0700

    finish migration, model, and eloquent

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [
+        'id'
+    ];
+
+    public function ProductGalleries()
+    {
+        return $this->hasMany(ProductGallery::class);
+    }
+
+    public function TransactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
+}
